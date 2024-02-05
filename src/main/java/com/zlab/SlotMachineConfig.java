29f7a626e18f6cb337fe5ecb34c87f16884762bb
alzotov <azotov@hotmail.com>
2024-02-05 19:49:15 +0800

    toStrings

--- src/main/java/com/zlab/SlotMachineConfig.java
+++ src/main/java/com/zlab/SlotMachineConfig.java
@@ -15,12 +15,21 @@ public class SlotMachineConfig {
         public SymbolType type;
         public String impact; // Only applicable for bonus symbols
         public Integer extra; // Only applicable for bonus symbols
+        @Override
+        public String toString() {
+            return "Symbol [reward_multiplier=" + reward_multiplier + ", type=" + type + ", impact=" + impact
+                    + ", extra=" + extra + "]";
+        }
     }
 
     public static class Probability {
         public int column;
         public int row;
         public Map<String, Integer> symbols;
+        @Override
+        public String toString() {
+            return "Probability [column=" + column + ", row=" + row + ", symbols=" + symbols + "]";
+        }
     }
 
     public static class WinCombination {
@@ -29,6 +38,11 @@ public class SlotMachineConfig {
         public int count;
         public String group;
         public List<List<String>> covered_areas; // Only applicable for some win combinations
+        @Override
+        public String toString() {
+            return "WinCombination [reward_multiplier=" + reward_multiplier + ", when=" + when + ", count=" + count
+                    + ", group=" + group + ", covered_areas=" + covered_areas + "]";
+        }
     }
 
     public int columns;
@@ -36,4 +50,9 @@ public class SlotMachineConfig {
     public Map<String, Symbol> symbols;
     public Map<String, List<Probability>> probabilities;
     public Map<String, WinCombination> win_combinations;
+    @Override
+    public String toString() {
+        return "SlotMachineConfig [columns=" + columns + ", rows=" + rows + ", symbols=" + symbols + ", probabilities="
+                + probabilities + ", win_combinations=" + win_combinations + "]";
+    }
 }
